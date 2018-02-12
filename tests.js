33c2c627602fa0a88b30d3e3c625f28a0406a0fe
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-02-12 18:32:19 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-var index = Susan
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
@@ -8,7 +8,7 @@ var index = Susan
 
 
 
-var height = 70
+if (index.height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
